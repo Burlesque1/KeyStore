d8b005318c30a20f2e26d11bcea0e0157b817ec7
しんらてんせい <123176000@qq.com>
2022-04-21 21:54:50 +0800

    merge

---
+++ app/src/main/java/com/example/myapplication/sss.java
@@ -0,0 +1,39 @@
+//package com.example.myapplication;
+//
+//import android.security.identity.IdentityCredentialStore;
+//
+//public abstract class sss  {
+//    public sss(){
+//        super();
+//    }
+
+
+//@NonNull
+//@Override
+//public String[] getSupportedDocTypes() {
+//        return new String[0];
+//        }
+//
+//@Nullable
+//@Override
+//public IdentityCredential getCredentialByName(@NonNull String s, int i) throws CipherSuiteNotSupportedException {
+//        return null;
+//        }
+//
+//@Nullable
+//@Override
+//public byte[] deleteCredentialByName(@NonNull String s) {
+//        return new byte[0];
+//        }
+//
+//@NonNull
+//@Override
+//public WritableIdentityCredential createCredential(@NonNull String s, @NonNull String s1) throws AlreadyPersonalizedException, DocTypeNotSupportedException {
+//        return null;
+//        }
+//
+//@Override
+//public boolean equals(@Nullable Object obj) {
+//        return super.equals(obj);
+//        }
+//}
